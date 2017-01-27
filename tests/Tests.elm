363fa3782f7c5b5cc7b945419d9c09560fac07d7
Matthew Griffith <mdg.griffith@gmail.com>
2017-01-27 14:54:34 -0500

    Basic easing function conversion testing

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,6 +4,43 @@ import Test exposing (..)
 import Expect
 import Fuzz exposing (list, int, tuple, string)
 import String
+import Ease exposing (Easing)
+import Curve
+
+
+easings : List ( Easing, String )
+easings =
+    [ ( Ease.inSine, "inSine" )
+      --, ( Ease.outSine, "outSine" )
+      --, ( Ease.inOutSine, "inOutSine" )
+      --, ( Ease.inQuad, "inQuad" )
+      --, ( Ease.outQuad, "outQuad" )
+      --, ( Ease.inOutQuad, "inOutQuad" )
+      --, ( Ease.inCubic, "inCubic" )
+      --, ( Ease.outCubic, "outCubic" )
+      --, ( Ease.inOutCubic, "inOutCubic" )
+      --, ( Ease.inQuart, "inQuart" )
+      --, ( Ease.outQuart, "outQuart" )
+      --, ( Ease.inOutQuart, "inOutQuart" )
+      --, ( Ease.inQuint, "inQuint" )
+      --, ( Ease.outQuint, "outQuint" )
+      --, ( Ease.inOutQuint, "inOutQuint" )
+      --, ( Ease.inExpo, "inExpo" )
+      --, ( Ease.outExpo, "outExpo" )
+      --, ( Ease.inOutExpo, "inOutExpo" )
+      --, ( Ease.inCirc, "inCirc" )
+      --, ( Ease.outCirc, "outCirc" )
+      --, ( Ease.inOutCirc, "inOutCirc" )
+      --, ( Ease.inBack, "inBack" )
+      --, ( Ease.outBack, "outBack" )
+      --, ( Ease.inOutBack, "inOutBack" )
+      --, ( Ease.inElastic, "inElastic" )
+      --, ( Ease.outElastic, "outElastic" )
+      --, ( Ease.inOutElastic, "inOutElastic" )
+      --, ( Ease.inBounce, "inBounce" )
+      --, ( Ease.outBounce, "outBounce" )
+      --, ( Ease.inOutBounce, "inOutBounce" )
+    ]
 
 
 all : Test
@@ -34,4 +71,31 @@ all =
                 \s1 s2 ->
                     s1 ++ s2 |> String.length |> Expect.equal (String.length s1 + String.length s2)
             ]
+        , describe "Easing Fns == Easing Functions converted to a curve "
+            (List.map testEasingCurveEquality easings)
         ]
+
+
+testEasingCurveEquality : ( Easing, String ) -> Test
+testEasingCurveEquality ( easeFn, name ) =
+    let
+        curve =
+            Curve.fromEasing easeFn
+
+        samples =
+            List.range 0 1000
+                |> (List.map (\x -> toFloat x / 1000))
+
+        equalPoints x =
+            test ("point " ++ toString x) <|
+                \() ->
+                    let
+                        delta =
+                            abs <|
+                                (Tuple.second <| Curve.pointAt curve x)
+                                    - (easeFn x)
+                    in
+                        Expect.atMost 0.001 delta
+    in
+        describe (name ++ " curve conversion test")
+            (List.map equalPoints samples)
